2451912c890d9617fb8e25b5f16b30a774501351
jlarryc <48612362+jlarryc@users.noreply.github.com>
2022-02-12 23:57:28 -0600

    Add files via upload

---
+++ CreatePublishUsingStreamlit/my_streamlit1.py
@@ -0,0 +1,16 @@
+'''
+This is a test of Streamlit
+
+
+
+'''
+
+import streamlit as st
+
+st.title('Our First Streamlit App')
+st.subheader('Introducting Streamlit in Automate Everything in Python')
+st.write('''
+This is our first Streamlit Web App.
+Enjoy it!
+
+''')
